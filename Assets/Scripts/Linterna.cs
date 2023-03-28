8c5a6ed35f7739920d4392a09308038731a00e61
CristopherNicolas <85201838+CristopherNicolas@users.noreply.github.com>
2023-03-27 23:53:36 -0300

    task list + linterna

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+public class Linterna : MonoBehaviour
+{
+    public float bateria=100;
+    
+  public void ActivarDesactivar()
+  {
+    //desactivar solo la luz para poder seguir controlando este script
+
+  }
+  //Apuntar
+  public void Update()
+  {
+    //raycast mientras este prendida
+    
+  }
+}
