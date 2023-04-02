4b125e64859db78f9a6f7c1fb2d2dadfd0e3e6b6
CristopherNicolas <85201838+CristopherNicolas@users.noreply.github.com>
2023-04-01 22:58:02 -0300

    v 0.1
    version con la linterna y la escena de debug y game actaulizadas

--- Assets/Scripts/Linterna.cs
+++ Assets/Scripts/Linterna.cs
@@ -1,18 +1,36 @@
-
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 public class Linterna : MonoBehaviour
 {
     public float bateria=100;
-    
-  public void ActivarDesactivar()
-  {
-    //desactivar solo la luz para poder seguir controlando este script
+    [SerializeField,Tooltip("el radio de la linterza, spherecast")]private int radio;
+    [SerializeField,Tooltip("Enemigos que estan en colicion con la linterna")] private List<GameObject> enemigosEnEscena;
+    [SerializeField,Tooltip("luz de la linterna")] Light luzLinterna;
 
-  }
-  //Apuntar
-  public void Update()
+    public void ActivarDesactivar()
   {
-    //raycast mientras este prendida
-    
+        //desactivar solo la luz para poder seguir controlando este script
+        luzLinterna.enabled = luzLinterna.enabled ? false : true;
+        //agregar aqui sonido de apagado o encendido
   }
+    //Controlar los enemigos  que estan en el rango de la linterna
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemigo"))
+        {
+            print($"enemigo {other.name} colicionado,agregado a la lista");
+            enemigosEnEscena.Add(other.gameObject);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemigo")  && enemigosEnEscena.Contains(other.gameObject) )
+        {
+            print("sale el enemigo de la lista");
+            enemigosEnEscena.Remove(other.gameObject);  
+        }
+    }
 }
